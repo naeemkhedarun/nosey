a1998f35bd97ee7f6ff7fedf0a3bb3f3e98a6975
Naeem Khedarun <naeem.khedarun@gmail.com>
2013-11-10 16:31:07 +0000

    Removing binaries from master.

--- releases/agent/Update-Version.ps1
+++
@@ -1,7 +0,0 @@
-$noseyService = Get-Service "csnosey"
-
-if($noseyService)
-{
-    $noseyService.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped, [TimeSpan]::FromMinutes(1))
-}
-
